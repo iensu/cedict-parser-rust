273cb9797eeee7b927fe037756bcb253cc49d388
Jens Östlund <jostlund@gmail.com>
2021-04-25 00:00:21 +0200

    Pass file as argument

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use std::ffi::OsString;
+
 use cedict_parser::{parse_cedict, CedictEntry};
 
 pub mod lib;
 
 fn main() {
-    let contents = std::fs::read_to_string("cedict_ts.u8").unwrap();
+    let args: Vec<OsString> = std::env::args_os().collect();
+
+    let cedict_file_path = args.get(1).expect("You need to pass a file path!");
+
+    let contents = std::fs::read_to_string(cedict_file_path).expect("Failed to read file!");
 
     let entries: Vec<CedictEntry> = parse_cedict(&contents);
 
